d138f61898962401639ad2139dd8047a5fb22a6f
German A. Victoria P <germanvictoria20@gmail.com>
2021-07-31 11:56:19 -0500

    se inicia el proyecto

---
+++ src/main/java/com/example/sol_prueba/SolPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sol_prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolPruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sol_prueba/SolPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sol_prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
